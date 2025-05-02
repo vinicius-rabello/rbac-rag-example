54e93e7c28ffe3afec1a1fdfa86fa43e803b9836
viniciusrabello <viniciusrabello@matematica.ufrj.br>
2025-05-02 17:19:56 -0300

    added requirements.txt file

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from rag.pipeline import run_query
 
 if __name__ == "__main__":
-    user_role = "TI"
+    user_role = "Financeiro"
     query = "Onde é nosso foco de atuação e faturamento?"
     response = run_query(user_role, query)
-    print(f"""Query: {query}")
+    print(f"""Query: {query},
+            User Role: {user_role},)
             Response: {response}""")
